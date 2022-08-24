d006606f9dc1a2abfad8450bb9ed044e5df0c14b
gimmicks-u <is@pusan.ac.kr>
2022-08-24 23:04:57 +0900

    first commit

---
+++ first_project/src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AppService } from './app.service';
+
+@Controller('cats')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() Body,
+    @Param() param: { id: string; name: string },
+  ): string {
+    // console.log(req);
+    // console.log(Body);
+    console.log(param);
+    // return 'hello world';
+
+    return this.appService.getHello();
+  }
+}
